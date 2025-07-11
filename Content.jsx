50c675f29850fb002e9f9152c7eb6379ca2acc83
YazhiniSathishKumar <167332505+YazhiniSathishKumar@users.noreply.github.com>
2025-07-11 14:58:47 +0530

    Add files via upload

---
+++ Content.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import './Content.css'
+
+
+function Content() {
+  return (
+    <>
+    
+    <div className="box">
+   <h1 className="h4 ">
+     Chharjarsi 
+    </h1>
+     <h2 className="h2"><bold>34&deg;</bold></h2>
+  <p className="p">Mostly cloudy 28&deg;/ 35&deg;</p>
+  <p className="p1">Air quality: 65-Satisficatory</p>
+  </div>
+
+
+
+
+
+  
+  </>
+  )
+}
+
+export default Content
